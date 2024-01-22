ebbd5fe21f4bdee3bedb26f34d03a85c834552f0
Macarena <maquibarrios@gmail.com>
2024-01-22 18:49:50 -0300

    Challenge finalizado

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Table, Container, Row, Col, Dropdown } from 'react-bootstrap';
+import { Table, Container, Row, Col, Dropdown, Modal, Button } from 'react-bootstrap';
 import { FaEllipsisV } from 'react-icons/fa';
 import { format } from 'date-fns';
 
@@ -11,6 +11,7 @@ import './home.css';
 
 function Home() {
   const [storedValue, setValue] = useLocalStorage('project', []);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const navigate = useNavigate();
 
@@ -33,11 +34,37 @@ function Home() {
     navigate(`/edit/${id}`);
   };
 
-  const handleDelete = (id) => {
-    console.log('Delete clicked for ID:', id);
-    const updatedProjects = storedValue.filter(project => project.id !== id);
-    setValue(updatedProjects);
+  // Modal
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [projectToDelete, setProjectToDelete] = useState(null);
+
+  const handleShowDeleteModal = (id) => {
+    setProjectToDelete(id);
+    setShowDeleteModal(true);
   };
+
+  const handleCloseDeleteModal = () => {
+    setShowDeleteModal(false);
+    setProjectToDelete(null);
+  };
+
+  const handleDelete = (id) => {
+    handleShowDeleteModal(id);
+  };
+
+  const confirmDelete = () => {
+    console.log('Deleting project ID:', projectToDelete);
+    const updatedProjects = storedValue.filter((project) => project.id !== projectToDelete);
+    setValue(updatedProjects);
+    handleCloseDeleteModal();
+  };
+
+  // Search
+  const filteredProjects = storedValue.filter((project) => {
+    const projectName = project.projectName && project.projectName.trim().toLowerCase();
+    const searchTermLower = searchTerm.toLowerCase().trim();
+    return projectName && (projectName.includes(searchTermLower) || projectName === searchTermLower);
+  });
 
   return (
     <div className="home-container">
@@ -49,7 +76,14 @@ function Home() {
           <span className="projects-title">My Projects</span>
         </div>
         <div className="navbar-right">
-          <button className="btn btn-danger btn-sm" onClick={handleBackClick}><span>+ Add project</span></button>
+          <input
+            className="search-input"
+            type="text"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button className="btn btn-danger btn-sm btn-sm-responsive" onClick={handleBackClick}><span>+ Add project</span></button>
         </div>
       </div>
       {/* Contenedor para centrar el contenido */}
@@ -68,31 +102,115 @@ function Home() {
                 </tr>
               </thead>
               <tbody className="tbody">
-                {storedValue.map((value, index) => (
-                  <tr key={index} >
-                    <td >
-                      <div className="project-info">
-                        <span className="name">{value.projectName}</span>
-                        <p className="date">Creation date: {format(new Date(value.creationDate), 'dd/MM/yyyy HH:mm a')}</p>
-                      </div>
-                    </td>
-                    <td>
+                {searchTerm
+                  ? filteredProjects.map((value, index) => (
+                    <tr key={index}>
+                      <td >
+                        <div className="project-info">
+                          <span className="name">{value.projectName}</span>
+                          <p className="date">Creation date: {format(new Date(value.creationDate), 'dd/MM/yyyy HH:mm a')}</p>
+                        </div>
+                      </td>
+                      <td>
+                        <div className='info-container'>
+                          <img
+                            src={thumbnailImage} alt="Assigned To" className="thumbnail" />
+                          {value.projectManager}
+                        </div>
+                      </td>
+                      <td>
+                        <div className='info-container'>
+                          <img src={thumbnailImage} alt="Assigned To" className="thumbnail" />
+                          {value.assignedTo}
+                        </div>
+                      </td>
+                      <td>{value.status}</td>
+                      <td>
+                        <button
+                          type="button"
+                          className="btn btn-light option-btn"
+                          onClick={() => handleToggleMenu(index)}
+                          aria-expanded={menuOpen[index]}
+                          aria-controls={`project-options-menu-${value.id}`}
+                        >
+                          <FaEllipsisV />
+                        </button>
+                        {menuOpen[index] && (
+                          <Dropdown.Menu show={menuOpen[index]} id={`project-options-menu-${value.id}`}>
+                            <Dropdown.Item onClick={() => handleEdit(value.id)}>Edit</Dropdown.Item>
+                            <Dropdown.Item onClick={() => handleDelete(value.id)}>Delete</Dropdown.Item>
+                          </Dropdown.Menu>
+                        )}
+                      </td>
+                    </tr>
+                  ))
+                  : storedValue.map((value, index) => (
+                    <tr key={index} >
+                      <td >
+                        <div className="project-info">
+                          <span className="name">{value.projectName}</span>
+                          <p className="date">Creation date: {format(new Date(value.creationDate), 'dd/MM/yyyy HH:mm a')}</p>
+                        </div>
+                      </td>
+                      <td>
+                        <div className='info-container'>
+                          <img
+                            src={thumbnailImage} alt="Assigned To" className="thumbnail" />
+                          {value.projectManager}
+                        </div>
+                      </td>
+                      <td>
+                        <div className='info-container'>
+                          <img src={thumbnailImage} alt="Assigned To" className="thumbnail" />
+                          {value.assignedTo}
+                        </div>
+                      </td>
+                      <td>{value.status}</td>
+                      <td>
+                        <button
+                          type="button"
+                          className="btn btn-light option-btn"
+                          onClick={() => handleToggleMenu(index)}
+                          aria-expanded={menuOpen[index]}
+                          aria-controls={`project-options-menu-${value.id}`}
+                        >
+                          <FaEllipsisV />
+                        </button>
+                        {menuOpen[index] && (
+                          <Dropdown.Menu show={menuOpen[index]} id={`project-options-menu-${value.id}`}>
+                            <Dropdown.Item onClick={() => handleEdit(value.id)}>Edit</Dropdown.Item>
+                            <Dropdown.Item onClick={() => handleDelete(value.id)}>Delete</Dropdown.Item>
+                          </Dropdown.Menu>
+                        )}
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </Table>
+          </Col>
+          {/* Fin Columna para pantallas medianas y grandes */}
+          {/* Columna para pantallas pequeñas */}
+          <Col xs={12} md={8} className="custom-col d-block d-md-none mx-0 mt-5" >
+            <ul className="project-list">
+              {/* Lista de proyectos */}
+              {searchTerm
+                ? filteredProjects.map((value, index) => (
+                  <li key={index} className="project-list-item">
+                    <div>
+                      <h3>{value.projectName}</h3>
+                      <p className="date">Creation date: {format(new Date(value.creationDate), 'dd/MM/yyyy HH:mm a')}</p>
                       <div className='info-container'>
                         <img
-                          src={thumbnailImage} alt="Assigned To" className="thumbnail" />
-                        {value.projectManager}
+                          src={thumbnailImage}
+                          alt="Thumbnail"
+                          className="thumbnail"
+                        />
+                        <p className="assigned-to">{value.assignedTo}</p>
                       </div>
-                    </td>
-                    <td>
-                      <div className='info-container'>
-                        <img src={thumbnailImage} alt="Assigned To" className="thumbnail" />
-                        {value.assignedTo}
-                      </div>
-                    </td>
-                    <td>{value.status}</td>
-                    <td>
-                      <button
-                        type="button"
+                    </div>
+
+                    <div className="project-list-item-options">
+                      <button type="button"
                         className="btn btn-light option-btn"
                         onClick={() => handleToggleMenu(index)}
                         aria-expanded={menuOpen[index]}
@@ -106,54 +224,62 @@ function Home() {
                           <Dropdown.Item onClick={() => handleDelete(value.id)}>Delete</Dropdown.Item>
                         </Dropdown.Menu>
                       )}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          </Col>
-          {/* Fin Columna para pantallas medianas y grandes */}
-          {/* Columna para pantallas pequeñas */}
-          <Col xs={12} md={8} className="custom-col d-block d-md-none mx-0 mt-5" >
-            <ul className="project-list">
-              {/* Lista de proyectos */}
-              {storedValue.map((value, index) => (
-                <li key={index} className="project-list-item">
-                  <div >
-                    <h3>{value.projectName}</h3>
-                    <p className="date">Creation date: {format(new Date(value.creationDate), 'dd/MM/yyyy HH:mm a')}</p>
-                    <div className='info-container'>
-                      <img
-                        src={thumbnailImage}
-                        alt="Thumbnail"
-                        className="thumbnail"
-                      />
-                      <p className="assigned-to">{value.assignedTo}</p>
                     </div>
-                  </div>
+                  </li>
+                ))
+                : storedValue.map((value, index) => (
+                  <li key={index} className="project-list-item">
+                    <div>
+                      <h3>{value.projectName}</h3>
+                      <p className="date">Creation date: {format(new Date(value.creationDate), 'dd/MM/yyyy HH:mm a')}</p>
+                      <div className='info-container'>
+                        <img
+                          src={thumbnailImage}
+                          alt="Thumbnail"
+                          className="thumbnail"
+                        />
+                        <p className="assigned-to">{value.assignedTo}</p>
+                      </div>
+                    </div>
 
-                  <div className="project-list-item-options">
-                    <button type="button"
-                      className="btn btn-light option-btn"
-                      onClick={() => handleToggleMenu(index)}
-                      aria-expanded={menuOpen[index]}
-                      aria-controls={`project-options-menu-${value.id}`}
-                    >
-                      <FaEllipsisV />
-                    </button>
-                    {menuOpen[index] && (
-                      <Dropdown.Menu show={menuOpen[index]} id={`project-options-menu-${value.id}`}>
-                        <Dropdown.Item onClick={() => handleEdit(value.id)}>Edit</Dropdown.Item>
-                        <Dropdown.Item onClick={() => handleDelete(value.id)}>Delete</Dropdown.Item>
-                      </Dropdown.Menu>
-                    )}
-                  </div>
-                </li>
-              ))}
+                    <div className="project-list-item-options">
+                      <button type="button"
+                        className="btn btn-light option-btn"
+                        onClick={() => handleToggleMenu(index)}
+                        aria-expanded={menuOpen[index]}
+                        aria-controls={`project-options-menu-${value.id}`}
+                      >
+                        <FaEllipsisV />
+                      </button>
+                      {menuOpen[index] && (
+                        <Dropdown.Menu show={menuOpen[index]} id={`project-options-menu-${value.id}`}>
+                          <Dropdown.Item onClick={() => handleEdit(value.id)}>Edit</Dropdown.Item>
+                          <Dropdown.Item onClick={() => handleDelete(value.id)}>Delete</Dropdown.Item>
+                        </Dropdown.Menu>
+                      )}
+                    </div>
+                  </li>
+                ))}
             </ul>
           </Col>
           {/* Fin Columna para pantallas pequeñas */}
         </Row>
+        <Modal show={showDeleteModal} onHide={handleCloseDeleteModal} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>Confirm Delete</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Are you sure you want to delete this project?
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseDeleteModal}>
+              Cancel
+            </Button>
+            <Button variant="danger" onClick={confirmDelete}>
+              Delete
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </Container>
     </div >
   );
